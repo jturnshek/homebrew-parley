ae3ced6c3e84f72dfa9a3f75e70662eda9457f73
James Turnshek <turnshek@gmail.com>
2026-08-29 07:20:35 -0400

    Update Parley Homebrew livecheck

--- Casks/parley.rb
+++ Casks/parley.rb
@@ -2,11 +2,17 @@ cask "parley" do
   version "2026.8.29-3"
   sha256 "4b890d210251c4bfdf912b13cdb8e68a3d4bf687741a839f6b063b3f06adb08f"
 
-  url "https://downloads.parley.im/parley/releases/v#{version}/darwin-arm64/parley-#{version}-darwin-arm64.tar.gz",
-      verified: "downloads.parley.im/"
+  url "https://downloads.parley.im/parley/releases/v#{version}/darwin-arm64/parley-#{version}-darwin-arm64.tar.gz"
   name "Parley"
   desc "Communicate with other people through software agents"
   homepage "https://parley.im/"
+
+  livecheck do
+    url "https://downloads.parley.im/parley/channels/homebrew.json"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
 
   depends_on arch: :arm64
 
