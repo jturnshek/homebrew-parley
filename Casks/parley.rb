47f192b4d4678c176aa2210ca4ad6e7d834171c6
James Turnshek <turnshek@gmail.com>
2026-08-30 13:38:12 -0400

    Update Parley to 2026.8.30-5

--- Casks/parley.rb
+++ Casks/parley.rb
@@ -1,6 +1,6 @@
 cask "parley" do
-  version "2026.8.29-10"
-  sha256 "51a9071ad0bec139883103bfde485b9debef2fcafa4ac730e6c560f5b6224388"
+  version "2026.8.30-5"
+  sha256 "94900642159c80d11009c2abbe740d11bd6fbd132d9ab73e9a035a800feaa428"
 
   url "https://downloads.parley.im/parley/releases/v#{version}/darwin-arm64/parley-#{version}-darwin-arm64.tar.gz"
   name "Parley"
